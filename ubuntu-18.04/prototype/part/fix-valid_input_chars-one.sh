75dc36a2e640a58ea60b5711de1379d3d2b9d6ae
samwhelp <samwhelp@users.noreply.github.com>
2018-09-19 12:58:19 +0800

    remove -line

--- ubuntu-18.04/prototype/part/fix-valid_input_chars-one.sh
+++ ubuntu-18.04/prototype/part/fix-valid_input_chars-one.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 
 
-sqlite3 -line dayi4.db <<EOF
+sqlite3 dayi4.db <<EOF
 UPDATE ime
 SET val='1234567890-abcdefghijklmnopqrstuvwxyz,./;''[]\\\`-~!@#$%^&*()_+{}|:"<>?='
 WHERE attr='valid_input_chars'
